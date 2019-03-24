d111ad58f7e6565140a29a9894a7e72fd3be0480
Tomas Nilsson <tomas.nilsson78@gmail.com>
2019-03-24 23:49:04 +0100

    Implemented Bubblesort

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -8,6 +8,22 @@ namespace Sorting
 {
     class Program
     {
+        public static void Bubblesort(List<int> list)
+        {
+            for(int i = 0; i < list.Count - 1; i++)
+            {
+                for(int j=0;j<list.Count-i-1;j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             List<int> tallista = new List<int>();
@@ -16,7 +32,7 @@ namespace Sorting
             tallista.Add(1);
             tallista.Add(9);
             tallista.Add(5);
-            tallista.Sort();
+            Bubblesort(tallista);
             foreach(int element in tallista)
             {
                 Console.Write(element + " ");
