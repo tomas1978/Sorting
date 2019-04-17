ec25b8f34460ae5c3a218e6a2b31e898c675abcf
tomas1978 <tomas.nilsson78@gmail.com>
2019-04-17 15:04:19 +0200

    Added implementation of SelectionSort and testing it in main function on short unsorted list

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -24,6 +24,34 @@ namespace Sorting
             }
         }
 
+        public static void SelectionSort(List<int> list)
+        {
+            //pos_min is short for position of min
+            int pos_min, temp;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                pos_min = i;//set pos_min to the current index of array
+
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[pos_min])
+                    {
+                        //pos_min will keep track of the index that min is in, this is needed when a swap happens
+                        pos_min = j;
+                    }
+                }
+
+                //if pos_min no longer equals i than a smaller value must have been found, so a swap must occur
+                if (pos_min != i)
+                {
+                    temp = list[i];
+                    list[i] = list[pos_min];
+                    list[pos_min] = temp;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             List<int> tallista = new List<int>();
@@ -32,7 +60,7 @@ namespace Sorting
             tallista.Add(1);
             tallista.Add(9);
             tallista.Add(5);
-            Bubblesort(tallista);
+            SelectionSort(tallista);
             foreach(int element in tallista)
             {
                 Console.Write(element + " ");
